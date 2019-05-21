4d43dc5a4a87d5961009eee6a9b7714a140c3dd0
Izzy <me@noahlesko.com>
2019-05-21 06:13:52 -0500

    impl trait for display as well as adding derive for basic missing functionality. started some basic testing work

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,35 @@
+use std::fmt;
+
+#[derive(Debug, Eq, PartialEq, Clone, Copy)]
 pub enum Genders {
     Male,
     Female,
+}
+
+impl fmt::Display for Genders {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let out = match self {
+            Genders::Male => "Male",
+            Genders::Female => "Female",
+        };
+
+        write!(f, "{}", out)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn equivalence_test() {
+        assert_eq!(Genders::Male, Genders::Male);
+        assert_eq!(Genders::Female, Genders::Female);
+    }
+
+    #[test]
+    fn display_test() {
+        assert_eq!(String::from("Male"), format!("{}", Genders::Male));
+        assert_eq!(String::from("Female"), format!("{}", Genders::Female));
+    }
 }
